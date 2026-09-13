530f60bc41680d13277536165067d7ab16b0fce0
Talib-ILM <musa123cool@gmail.com>
2026-09-14 01:02:43 +0500

    use logo as favicon via app/icon.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,6 @@ const sans = Plus_Jakarta_Sans({
 
 export const metadata: Metadata = {
   title: { default: "RABS Portal", template: "%s · RABS Portal" },
-  icons: { icon: "/logo.png" },
 };
 
 export default function PortalRootLayout({
